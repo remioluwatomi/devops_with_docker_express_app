9e7270679e66ffcd113db75c43cbcf1acb868cac
remy_oluwatomi <remioluwatomicephas@gmail.com>
2025-05-07 19:07:13 -0400

    test: included a test endpoint to test watchtower locally

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
+app.get("/test", (req, res)=> {
+  res.json({
+    "devops_with_docker": "true",
+    "name": "tomi"
+  })
+})
+
 const PORT = 8080
 
 app.listen(PORT, () => {
